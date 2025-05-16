59a4850ae42decc2a3394a06f2f50611591af96e
Ethan Balling <ethanballing@gmail.com>
2025-05-16 10:03:35 -0600

    Add collision detection to circleshape.py and check for collision each frame of game loop.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -35,6 +36,11 @@ def main():
         #player.update(dt)
         # Update all sprites in the updatable group
         updatable.update(dt)
+        
+        for asteroid in asteroids:
+            if player.check_collision(asteroid):
+                print("Game Over!")
+                sys.exit()
         
         # Screen clearing
         screen.fill("black")
